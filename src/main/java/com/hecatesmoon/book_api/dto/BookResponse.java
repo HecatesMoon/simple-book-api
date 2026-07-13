87443b33ad21f3b3e24cc86bb912e619d2074860
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-13 13:33:51 -0400

    chore: change folder name

---
+++ src/main/java/com/hecatesmoon/book_api/dto/BookResponse.java
@@ -0,0 +1,33 @@
+package com.hecatesmoon.book_api.dto;
+
+import com.hecatesmoon.book_api.model.Book;
+
+public class BookResponse {
+    
+    private String title;
+    private String author;
+    private Integer pages;
+
+    public BookResponse(){}
+
+    public static BookResponse from (Book book){
+        BookResponse bookResponse = new BookResponse();
+        bookResponse.title = book.getTitle();
+        bookResponse.author = book.getAuthor();
+        bookResponse.pages = book.getPages();
+        return bookResponse;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+}
