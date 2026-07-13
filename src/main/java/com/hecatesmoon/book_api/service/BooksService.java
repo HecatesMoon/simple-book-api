87206f708e6896eb1d2a05b49319d216c076e6b6
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-13 13:34:21 -0400

    feat: create books service with basic functions

---
+++ src/main/java/com/hecatesmoon/book_api/service/BooksService.java
@@ -0,0 +1,35 @@
+package com.hecatesmoon.book_api.service;
+
+import java.util.List;
+
+import com.hecatesmoon.book_api.dto.BookRequest;
+import com.hecatesmoon.book_api.dto.BookResponse;
+import com.hecatesmoon.book_api.model.Book;
+import com.hecatesmoon.book_api.repository.BooksRepository;
+
+public class BooksService {
+    
+    private final BooksRepository booksRepository;
+
+    public BooksService (BooksRepository booksRepository){
+        this.booksRepository = booksRepository;
+    }
+
+    List<BookResponse> findAll(){
+        return booksRepository.findAll().stream().map(BookResponse::from).toList();
+    }
+
+    BookResponse findById(Long id){
+        Book book = booksRepository.findById(id).orElseThrow();
+        return BookResponse.from(book);
+    }
+
+    BookResponse create(BookRequest r){
+        Book book = booksRepository.save(BookRequest.toEntity(r));
+        return BookResponse.from(book);
+    }
+
+    void delete(Long id){
+        booksRepository.deleteById(id);
+    }
+}
