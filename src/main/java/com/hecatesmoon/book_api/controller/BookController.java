c20268efe27178fd24cd47f3a2547af57a20c0ca
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-19 16:20:52 -0400

    feat: create book controller

---
+++ src/main/java/com/hecatesmoon/book_api/controller/BookController.java
@@ -0,0 +1,60 @@
+package com.hecatesmoon.book_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hecatesmoon.book_api.dto.BookRequest;
+import com.hecatesmoon.book_api.dto.BookResponse;
+import com.hecatesmoon.book_api.service.BooksService;
+
+import jakarta.validation.Valid;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BooksService booksService;
+
+    BookController (BooksService booksService){
+        this.booksService = booksService;
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+        
+        return ResponseEntity.ok(booksService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookResponse> getBook(@PathVariable Long id){
+        return ResponseEntity.ok(booksService.findById(id));
+    }
+    
+    @PostMapping("")
+    public ResponseEntity<BookResponse> addBook(@Valid @RequestBody BookRequest bookRequest) {
+        BookResponse newBook = booksService.create(bookRequest);
+        return ResponseEntity.ok(newBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteBook(@PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "the book (id "+id+") was deleted succesfully");
+        return ResponseEntity.ok(response);
+    }
+    
+}
