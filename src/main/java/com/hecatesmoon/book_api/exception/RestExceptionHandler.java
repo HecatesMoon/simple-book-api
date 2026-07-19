ac0c812f6909dfa774be509fe8cd165825720d3c
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-19 13:05:25 -0400

    feat: add exception handler for MethodRgumentNotValidException

--- src/main/java/com/hecatesmoon/book_api/exception/RestExceptionHandler.java
+++ src/main/java/com/hecatesmoon/book_api/exception/RestExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.hecatesmoon.book_api.exception;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,4 +24,19 @@ public class RestExceptionHandler {
         response.put("error", ex.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException (MethodArgumentNotValidException ex){
+        
+        Map<String, String> responseErrors = new HashMap<>();
+        for (FieldError error: ex.getBindingResult().getFieldErrors()){
+            responseErrors.put(error.getField(), error.getDefaultMessage());
+        }
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "field validation error");
+        response.put("fieldErrors", responseErrors);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
 }
