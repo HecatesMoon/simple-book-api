248264b9188ba004f6480d14e1a9e663484e8d4c
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-07-19 16:26:50 -0400

    feat: add id field to BookResponse

--- src/main/java/com/hecatesmoon/book_api/dto/BookResponse.java
+++ src/main/java/com/hecatesmoon/book_api/dto/BookResponse.java
@@ -4,6 +4,7 @@ import com.hecatesmoon.book_api.model.Book;
 
 public class BookResponse {
     
+    private Long id;
     private String title;
     private String author;
     private Integer pages;
@@ -12,10 +13,15 @@ public class BookResponse {
 
     public static BookResponse from (Book book){
         BookResponse bookResponse = new BookResponse();
+        bookResponse.id = book.getId();
         bookResponse.title = book.getTitle();
         bookResponse.author = book.getAuthor();
         bookResponse.pages = book.getPages();
         return bookResponse;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getTitle() {
